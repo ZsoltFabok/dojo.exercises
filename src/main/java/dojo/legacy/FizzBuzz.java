8c3de17b02b6f6645874df5311aab685dec7e7d4
Zsolt Fabok <me@zsoltfabok.com>
2012-05-31 14:17:09 +0300

    first commit

---
+++ src/main/java/dojo/legacy/FizzBuzz.java
@@ -0,0 +1,25 @@
+package dojo.legacy;
+
+public class FizzBuzz {
+	 public String[] play(int start, int stop) {
+		 if (start > 0 && start <= stop) {
+		     int length = stop - start + 1;
+		     String[] array = new String[length];
+		     for (int i = 0; i < length; i++) {
+		         if ((start + i) % 15 == 0) {
+		                 array[i]="Fizzbuzz";
+		         } else
+		                 if (start + i % 5 == 0) {
+		                 array[i]="Buzz";
+		                 } else { if ((start + i) % 3 == 0)
+		                 array[i]="Fizz";
+		         else {
+		                 array[i] =  "" + (start + i);
+		         }}
+		     }
+		     return array;
+		 } else {
+		     throw new IllegalArgumentException();
+		 }
+	 }
+}
