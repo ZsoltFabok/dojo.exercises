7d5b8cc0b6623a333657fd4f84461bc075283c14
Zsolt Fabok <me@zsoltfabok.com>
2012-06-06 16:29:28 +0300

    New exercise description

--- src/main/java/dojo/basics/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This exercise is designed to start mastering the basics skills necessary for TDD.
- *
- * As a software developer I want a class, which provides me the possibility
- * to play the famous "Fizz Buzz" game. I want to give a range of numbers as input
- * and in return I want numbers following the "Fizz Buzz" rule.
- */
-package dojo.basics;
