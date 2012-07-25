a144ab2973a866c0ee4f4a57a6adb0eec6729bfe
Zsolt Fabok <me@zsoltfabok.com>
2012-07-25 19:08:28 +0300

    one possible solution

--- src/main/java/dojo/legacy/currencyformat/HungarianCurrencyFormatter.java
+++ src/main/java/dojo/legacy/currencyformat/HungarianCurrencyFormatter.java
@@ -1,26 +1,40 @@
 package dojo.legacy.currencyformat;
 
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class HungarianCurrencyFormatter {
 
+    private static Locale HUN = new Locale("hu", "HU");
+
+    public String format(String number) {
+        NumberFormat currencyFormatter =
+            NumberFormat.getCurrencyInstance(HUN);
+
+        String currencyFormattedNumber =
+            currencyFormatter.format(Double.parseDouble(number));
+
+        String formattedNumberWithProperGroupSeparator =
+            replaceLocaleSpecificGroupSeparatorWithSpace(currencyFormattedNumber);
+
+        return removeFtSuffix(formattedNumberWithProperGroupSeparator);
+    }
+
+    private String removeFtSuffix(String number) {
+        return number.substring(0, number.indexOf(" Ft"));
+    }
+
+    private String replaceLocaleSpecificGroupSeparatorWithSpace(String number) {
+        char separator = DecimalFormatSymbols.getInstance(HUN).getGroupingSeparator();
+        return number.replace(separator, ' ');
+    }
+
     public static void main(String[] args) {
-        if (args.length == 1)
-        if (!args[0].contains(".")) {
-            char[] n = args[0].toCharArray();
-            final StringBuilder sb = new StringBuilder(String.copyValueOf(n));
-            for (int j = 1; j <= (n.length/3); j++)
-                if (n.length - 3 * j != 0) {
-                  sb.insert(n.length - 3 * j, " ");
-            }
-            System.out.println("Your number (" + args[0] + ") is written like this in Hungarian: \"" + sb.toString() + "\"");
-        } else {
-            StringBuffer sb = new StringBuffer();
-            final StringBuffer sb_2 = new StringBuffer(args[0].split("\\.")[0]);
-            for (int i = 1; i <= (args[0].split("\\.")[0].length() / 3); i++) {
-              if (args[0].split("\\.")[0].length() - 3 * i !=0)
-               sb_2.insert(args[0].split("\\.")[0].length()-3*(i), " ");
-            }
-            System.out.println("Your number (" + args[0] + ") is written like this in Hungarian: \"" + sb.append(sb_2.toString()).append(",").append(args[0].split("\\.")[1]).toString() + "\"");
-            return;
+        if (args.length == 1) {
+            HungarianCurrencyFormatter formatter = new HungarianCurrencyFormatter();
+            System.out.printf("Your number (%s) is written like this in Hungarian: \"%s\"%n",
+            args[0], formatter.format(args[0]));
         }
     }
 }
